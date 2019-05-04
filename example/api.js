8f18dba6db2b38369a5f694436c05ba624ab7b5e
David Brightley <david@brightleydeveloped.com>
2019-05-04 16:38:47 -0700

    Fixing issue with example

--- example/api.js
+++ example/api.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2018. Brightley Developed -- www.brightleydeveloped.com
  */
 
-const API = require('bookshelf-rest-api');
+const {API} = require('bookshelf-rest-api');
 const User = require('./models/user');
 
 module.exports = function(app) {
